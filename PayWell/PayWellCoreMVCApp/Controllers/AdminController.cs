29fa48587e0ebea193aded0d823557efc23a5bda
nahorkitsa <35925973+nahorkitsa@users.noreply.github.com>
2019-02-21 09:52:45 +0530

    Add files via upload

---
+++ PayWell/PayWellCoreMVCApp/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PayWellCoreMVCApp.Controllers
+{
+    [Route("Administrator")]
+    public class AdminController : Controller
+    {
+        [Route("Index")]
+        public String Index()
+        {
+            return "This is Admin index";
+        }
+
+        [Route("Product")]
+        public string AddProduct()
+        {
+            return "This is Admin Add Product Home Page";
+        }
+    }
+}
---
+++ PayWell/PayWellCoreMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PayWellCoreMVCApp.Models;
+
+namespace PayWellCoreMVCApp.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+       
+        public String Index(string id)
+        {
+            if (id == null)
+            {
+                return "Home Index :Hello...!!Your First MVC App";
+            }
+            return "HomeController :Index : id =" +id;
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
